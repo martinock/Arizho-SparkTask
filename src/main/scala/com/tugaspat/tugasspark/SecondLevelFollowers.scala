6964ab234d9a690e1f66588bf4797614e4d03a42
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2017-12-14 23:28:16 +0700

    fixing output

--- src/main/scala/com/tugaspat/tugasspark/SecondLevelFollowers.scala
+++ src/main/scala/com/tugaspat/tugasspark/SecondLevelFollowers.scala
@@ -4,15 +4,22 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
+import java.io._
+
 import scala.collection.mutable.ListBuffer
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 object SecondLevelFollowers {
+	@transient lazy val Logger = LoggerFactory.getLogger(this.getClass)
+
 	def main(args: Array[String]) {
 
 		val conf = new SparkConf().setAppName("Spark Arizho SecondLevelFollowers")
     	val sc = new SparkContext(conf)
 
-		val textFile = sc.textFile(args(1))
+		val textFile = sc.textFile(args(0))
 
 		def generateFirstKV(lines: Array[String]) : ListBuffer[(String,String)] = {
 			var temp_container = new ListBuffer[(String,String)]()
@@ -31,7 +38,6 @@ object SecondLevelFollowers {
 			val results = new ListBuffer[(String,String)]()
 			val list_of_followers = new ListBuffer[String]()
 			val list_of_following = new ListBuffer[String]()
-			println(tuple)
 			for (value <- tuple._2.split(" ")){
 				if (value.split(":")(0) == "follows") {
 					list_of_following += value.split(":")(1)
@@ -40,15 +46,11 @@ object SecondLevelFollowers {
 					list_of_followers += value.split(":")(1)
 				}
 			}
-			println(list_of_followers)
-			println(list_of_following)
 			for (follower <- list_of_followers){
 				for (following <- list_of_following) {
-					println(((following,follower)))
 					results += ((following,follower))
 				}
 			}
-			println(results)
 			return results
 		}
 
@@ -57,6 +59,7 @@ object SecondLevelFollowers {
 
 		val listTotalSecondFollower = listSecondFollowers.map(s => (s._1,s._2.split(" ").length)).sortBy (-_._2)
 
-		listTotalSecondFollower.take(10)
+		// Logger.info(listTotalSecondFollower.take(10).mkString("[", ",", "]"))
+		new PrintWriter("outputpacil.txt") { write(listTotalSecondFollower.take(10).mkString("[", ",", "]")); close }
 	}
 }
